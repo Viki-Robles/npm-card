35352cb6378cbcdce51e9ccf3f8edd47f288e98f
Vcky V <vasiliki.robles@gmail.com>
2021-04-23 12:47:07 +0100

    feat: add

--- bin/card.js
+++ bin/card.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const fs = require("fs");
 const path = require("path");
 const output = fs.readFileSync(path.join(__dirname, "output"), "utf8");
--- build.js
+++ build.js
@@ -7,8 +7,8 @@ const path = require("path");
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white("Vicky Robles/"),
-  handle: chalk.cyan("MissVRobles"),
+  name: chalk.white("Vicky Robles - "),
+  handle: chalk.cyan("blueMonkey"),
   dev: chalk.cyan("https://dev.to/vikirobles"),
   work: chalk.white("Front-end Engineer"),
   github: chalk.cyan("https://github.com/Viki-Robles"),
@@ -16,13 +16,12 @@ const data = {
     "https://www.linkedin.com/in/vicky-vasilopoulou-52230111b"
   ),
   web: chalk.cyan("https://myportfolio-puce.vercel.app"),
-  npm: chalk.white("npm-vrobles-card"),
-  labelWork: chalk.white.bold("      Work:"),
-  labelGitHub: chalk.white.bold("    GitHub:"),
-  labelLinkedin: chalk.white.bold("  Linkedin:"),
-  labelWeb: chalk.white.bold("       Web:"),
-  labelDev: chalk.white.bold("       DevCommunity:"),
-  labelCard: chalk.white.bold("      Card:"),
+  npm: chalk.white("vicky-vasilopoulou"),
+  labelWork: chalk.white.bold("          Work:"),
+  labelGitHub: chalk.white.bold("          GitHub:"),
+  labelLinkedin: chalk.white.bold("          Linkedin:"),
+  labelWeb: chalk.white.bold("          Web:"),
+  labelDev: chalk.white.bold("       Dev.to:"),
 };
 
 // Actual strings we're going to output
@@ -33,7 +32,6 @@ const githubing = `${data.labelGitHub}  ${data.github}`;
 const linkeding = `${data.labelLinkedin}  ${data.linkedin}`;
 const webing = `${data.labelWeb}  ${data.web}`;
 const deving = `${data.labelDev}  ${data.dev}`;
-const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output =
@@ -51,16 +49,14 @@ const output =
   deving +
   newline +
   newline +
-  carding;
-
-fs.writeFileSync(
-  path.join(__dirname, "bin/output"),
-  chalk.green(
-    boxen(output, {
-      padding: 1,
-      margin: 1,
-      borderStyle: "double",
-      borderColor: "white",
-    })
-  )
-);
+  fs.writeFileSync(
+    path.join(__dirname, "bin/output"),
+    chalk.green(
+      boxen(output, {
+        padding: 1,
+        margin: 1,
+        borderStyle: "double",
+        borderColor: "white",
+      })
+    )
+  );
